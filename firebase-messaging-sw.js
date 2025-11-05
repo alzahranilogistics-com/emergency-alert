c73c465f776fec1b338ddd511683c4cb82321360
alzahranilogistics-com <alzahranilogistics@gmail.com>
2025-11-05 15:00:37 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,11 @@
-importScripts("https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/12.5.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/12.5.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.5.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyAgSfZgX13q4tK92T5_6hDR5pNKkhw8YeI",
   authDomain: "za122-789c0.firebaseapp.com",
   projectId: "za122-789c0",
+  storageBucket: "za122-789c0.firebasestorage.app",
   messagingSenderId: "139885324329",
   appId: "1:139885324329:web:2b8128ba318ef4da93d438"
 });
@@ -12,9 +13,11 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("📢 [Service Worker] رسالة بالخلفية:", payload);
-  self.registration.showNotification(payload.notification.title, {
+  console.log('📩 إشعار بالخلفية:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon
-  });
+    icon: '/icon.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
